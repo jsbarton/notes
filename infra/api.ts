f54ecd19468f249ed6179d70632246aab3b720bc
Jordan Barton <jordanbarton@Jordan-Barton-DYH20XJQ12.local>
2025-05-30 09:46:41 -0700

    create s3 bucket, hello world api

--- infra/api.ts
+++ infra/api.ts
@@ -1,7 +1,10 @@
 import { bucket } from "./storage";
 
-export const myApi = new sst.aws.Function("MyApi", {
-  url: true,
+export const api = new sst.aws.ApiGatewayV2("Api");
+
+// When this API is invoked, ths function called handler in
+// /packages/functions/src/api.ts will be executed.
+api.route("GET /", {
   link: [bucket],
-  handler: "packages/functions/src/api.handler"
+  handler: "packages/functions/src/api.handler",
 });
--- infra/storage.ts
+++ infra/storage.ts
@@ -1,1 +1,8 @@
-export const bucket = new sst.aws.Bucket("MyBucket");
+// Create an S3 bucket
+// An S3 bucket is a fundamental unit within Amazon S3,
+// AWS's object storage service. It acts as a container
+// for storing objects, which can be any type of data, such as
+// files, images, or even entire websites
+
+// We're using this bucket later to handle file uploads
+export const bucket = new sst.aws.Bucket("Uploads");
